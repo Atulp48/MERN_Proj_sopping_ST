43f484a8b4e8f64ed69c50ffdd72b99483387e63
atul <patelatul948@gmail.com>
2023-12-30 12:09:27 +0530

    third

--- server/database/db.js
+++ server/database/db.js
@@ -6,7 +6,7 @@ const Connection = async (USERNAME,PASSWORD) => {
     mongoose.set('strictQuery', true);
     const URL = `mongodb://${USERNAME}:${PASSWORD}@ac-anbmlb3-shard-00-00.vzfc2sq.mongodb.net:27017,ac-anbmlb3-shard-00-01.vzfc2sq.mongodb.net:27017,ac-anbmlb3-shard-00-02.vzfc2sq.mongodb.net:27017/?ssl=true&replicaSet=atlas-fsi7ew-shard-0&authSource=admin&retryWrites=true&w=majority`
     try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true});
+        await mongoose.connect(URL);
         console.log('Database Connected Succesfully');
     } catch(error) {
         console.log('Error: ', error.message);
